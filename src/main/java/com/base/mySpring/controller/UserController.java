28966ceac0f3f7f1c27feb2f8d899310b5204ec8
leejunsu <qkfhtit@gmail.com>
2022-07-29 14:50:35 +0900

    rest api

--- src/main/java/com/base/mySpring/controller/UserController.java
+++ src/main/java/com/base/mySpring/controller/UserController.java
@@ -4,6 +4,7 @@ import com.base.mySpring.dto.UserDto;
 import com.base.mySpring.entity.Account;
 import com.base.mySpring.entity.User;
 import com.base.mySpring.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +32,16 @@ public class UserController {
     @GetMapping("")
     public List<UserDto> getUsers(){
         return userService.selectUsers();
+    }
+
+    @PutMapping("/{id}")
+    public UserDto modifyUser(@PathVariable Long id, @RequestBody UserDto userDto){
+        return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> removeUser(@PathVariable Long id){
+        return userService.deleteUser(id);
     }
 
 
--- src/main/java/com/base/mySpring/service/UserService.java
+++ src/main/java/com/base/mySpring/service/UserService.java
@@ -9,9 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -59,6 +63,7 @@ public class UserService {
 
         @Transactional(readOnly = true)
         public List<UserDto> selectUsers(){
+
             List<User> userRepositoryAll = userRepository.findAll();
             List<UserDto> userDtoList = userRepositoryAll.stream()  // Stream<User>
                     .map(user -> modelMapper.map(user, UserDto.class)) // Stream<UserDto>
@@ -66,5 +71,34 @@ public class UserService {
             return userDtoList;
 
         }
+
+        public UserDto updateUser(Long id, UserDto userDto) {
+
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+            // setter 메서드를 호출 하면 update가 된다.
+            // @Transactional 선언 되있어야만 setter로만 update가능
+            user.setName(userDto.getName());
+            user.setEmail(userDto.getEmail());
+
+            UserDto existUserDto = modelMapper.map(user, UserDto.class);
+            return existUserDto;
+
+        }
+
+        public ResponseEntity<?> deleteUser(Long id){
+
+            Optional<User> optionalUser = userRepository.findById(id);
+
+            if(!optionalUser.isPresent()){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(id + " User Not Found");
+            }
+
+            User existUser = optionalUser.get();
+            userRepository.delete(existUser);
+            return ResponseEntity.status(HttpStatus.OK).body(id + "Delete Complete");
+        }
+
+
 
 }
